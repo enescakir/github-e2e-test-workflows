d7248e3d53ea76ca27dc43f3e1b01f59327809bc
Enes Cakir <enes@cakir.web.tr>
2024-06-29 15:03:13 +0300

    Test ubicloud test packages

---
+++ cache/go/go.mod
@@ -0,0 +1,5 @@
+module example/project
+
+go 1.22
+
+require github.com/enescakir/emoji v1.0.0
---
+++ cache/go/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+    "github.com/enescakir/emoji"
+)
+
+func main() {
+    fmt.Printf("Hello world %v\n", emoji.WavingHand)
+}
